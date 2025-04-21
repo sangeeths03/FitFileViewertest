93045c349ee73f5cc302769eec9906ae6f2b0ed9
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-04-21 10:43:52 -0400

    Remove old ESLint configuration files and add new .eslintrc.cjs with updated rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules: {
+    "no-console": "warn",
+    eqeqeq: "error"
+  }
+};
